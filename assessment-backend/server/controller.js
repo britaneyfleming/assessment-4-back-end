e26aad047dd29ce1147bad72da8486c3ae401542
Britaney Fleming <britaneyrfleming@gmail.com>
2022-12-03 16:30:07 -0600

    first commit

---
+++ assessment-backend/server/controller.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+    getCompliment: (req, res) => {
+        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
+      
+        // choose random compliment
+        let randomIndex = Math.floor(Math.random() * compliments.length);
+        let randomCompliment = compliments[randomIndex];
+      
+        res.status(200).send(randomCompliment);
+    }
+
+}
+    module.exports = {
+    getFortune: (req, res) => {
+        const fortune = ["A faithful friend is a strong defense.", "Advice, when most needed, is least heeded.", "Protective measures will prevent costly disasters."];
+      
+        // choose random fortune
+        let randomIndex1 = Math.floor(Math.random() * fortune.length);
+        let randomFortune = fortune[randomIndex1];
+      
+        res.status(200).send(randomFortune);
+    }
+
+}
